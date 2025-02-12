6e3d961c9df1629a0b10f1a61ea1cca1416aa752
Ricardo <ricardoruasrumi@gmail.com>
2025-02-12 01:14:19 -0300

    18

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function handleClick(event) {
             startTimer();
         }
     } else {
-        // Bottom floor service
+        willBeepEvery30Seconds = false;
         if (isRunning) {
             clearInterval(timerId);
             resetTimer();
